5d1b20a6dcd73216fa8979b0952176e2f5026c88
BabieraWalter <walterbabiera3@gmail.com>
2021-02-25 18:47:33 -0300

    Agregando librerias y desarrolando los registros

---
+++ moduloadministracion.cpp
@@ -0,0 +1,52 @@
+#include<windows.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<locale.h>
+
+struct usuarios
+{
+	char Usuario[10];
+	char Contrasena[10];
+	char ApeNom[60];	
+};
+
+struct veterinario
+{
+	char ApeNom[60];
+	int Matricula;
+	int DNI;
+	char Telefono[25];
+};
+
+struct fecha
+{
+	int dd, mm, aa;
+};
+
+struct mascota
+{
+	char ApeNom[60];
+	char Domicilio[60];
+	int DNI_Dueno;
+	char Localidad[60];
+	fecha fechanacimiento;
+	float Peso;
+	char Telefono[25];
+};
+
+struct turnos
+{
+	int matriculaveterinario;
+	fecha fechaturno;
+	int DNI_Dueno;
+	char detalle[380];	
+	bool borrado;
+};
+
+bool verificacionnombre(int vou, char nombre[15]);
+bool verificacioncontrasena(char contra[35]);
+void registracionconfirmada(int vou, char nombre[15], char contra[35]);
+void atencionporveterinarios();
+void ranking();
+
